0d3478921aadcc71a2c195fa4a66a0ffb7246c6b
Matt MacAulay <matt@dulcepericulum.org>
2014-03-27 21:34:46 -0400

    Server ws over TLS

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+var fs = require('fs')
+var https = require('https')
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080})
+
+// Dummy request handler
+function processRequest(req, res) {
+  res.writeHead(200)
+  res.end('fdhveery')
+}
+
+var app = https.createServer({
+  key: fs.readFileSync('./dev-key.pem'),
+  cert: fs.readFileSync('./dev-cert.pem')
+}, processRequest).listen(8080)
+
+
+var wss = new WebSocketServer({server: app})
 
 wss.on('connection', function(ws) {
   console.log('Connected.')
