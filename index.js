1e1218218ebad66e196c9ff5b7e8d8660ced7e31
Derek Watson <watson@dcw.ca>
2014-03-31 20:44:07 -0400

    cleanup, added index.js, basic server test

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var program = require('commander')
+var pkg = require('./package.json')
+
+program
+  .version(pkg.version)
+  .option('-p, --port [port]', 'Server port [5000]', 5000)
+  .parse(process.argv)
+
+
+require('./lib/server').listen(program.port)
