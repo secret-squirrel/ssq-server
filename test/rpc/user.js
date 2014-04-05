b7c14df1795832997e06ba80f4a25c39d17fb5bd
Derek Watson <watson@dcw.ca>
2014-04-05 16:07:06 -0400

    rpc/user#get

--- test/rpc/user.js
+++ test/rpc/user.js
@@ -6,19 +6,44 @@ var rpcUser = require('../../lib/rpc/user')(mockUser, mockPublicKey, mockWs)
 
 describe('rpc/user', function() {
 
-  var allUserCount
+  var allUsers
   before(function(done) {
-    User.count().success(function(c) {
-      allUserCount = c
+    User.findAll().success(function(users) {
+      allUsers = users
       done()
     })
   })
 
-  it('returns all users', function(done) {
-    rpcUser.index({}, function(err, results) {
-      assert.ok(results)
-      assert.equal(allUserCount, results.length)
-      done()
+  describe('index', function() {
+    it('returns all users', function(done) {
+      rpcUser.index({}, function(err, results) {
+        assert.notOk(err)
+        assert.ok(results)
+        assert.equal(allUsers.length, results.length)
+        done()
+      })
+    })
+  })
+
+  describe('get', function() {
+    it('returns a single user', function(done) {
+      var expectedUser = allUsers[0]
+      rpcUser.get(expectedUser.id, function(err, result) {
+        assert.notOk(err)
+        assert.ok(result)
+        assert.equal(expectedUser.id, result.id)
+        assert.equal(expectedUser.name, result.name)
+        done()
+      })
+    })
+
+    it('returns an error for missing users', function(done) {
+      rpcUser.get(999, function(err, result) {
+        assert.ok(err)
+        assert.notOk(result)
+        assert.include(err, 'No such user')
+        done()
+      })
     })
   })
 })
